067ba92fa9a6722d3bdb51a8b4ae92265e476548
VSUPMM <valyafenris@gmail.com>
2020-01-07 17:04:22 +0300

    HM_updated

--- FishStore/src/main/java/ru/java/courses/fishstore/Store.java
+++ FishStore/src/main/java/ru/java/courses/fishstore/Store.java
@@ -9,6 +9,11 @@ public class Store {
 
     private HashMap<AbstractFish, Box[]> fishBoxes = new HashMap<>();
 
+    public Store(int budget, HashMap<AbstractFish, Box[]> fishBoxes) {
+        this.budget = budget;
+        this.fishBoxes = fishBoxes;
+    }
+
     public Box sell(AbstractFish requiedFish){
         Box box = removeFishBoxes(requiedFish);
         budget += box.getTotalPrice();
@@ -23,17 +28,25 @@ public class Store {
 
     public void addFishBoxes (Box box){
         Box[] boxes = fishBoxes.get(box.getFish());
-        boxes = Arrays.copyOf(boxes, boxes.length + 1);
+        if (boxes == null){
+            boxes = new Box[1];
+        } else {
+            boxes = Arrays.copyOf(boxes, boxes.length + 1);
+        }
         boxes[boxes.length-1] = box;
         fishBoxes.put(box.getFish(), boxes); //replace with new array
     }
 
     public Box removeFishBoxes (AbstractFish requiedFish){
         Box[] boxes = fishBoxes.get(requiedFish);
-        Box box = boxes[boxes.length - 1]; //last box
-        boxes = Arrays.copyOf(boxes, boxes.length - 1);
-        fishBoxes.put(box.getFish(), boxes);
-        return box;
+        if (boxes != null) {
+            Box box = boxes[boxes.length - 1]; //last box
+            boxes = Arrays.copyOf(boxes, boxes.length - 1);
+            fishBoxes.put(box.getFish(), boxes);
+            return box;
+        } else {
+            return null;
+        }
     }
 
 }
